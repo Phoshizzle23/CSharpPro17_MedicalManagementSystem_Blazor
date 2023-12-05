f8f758ce6c80d82b6d39b0ef43b726436239219c
Phoshizzle23 <mattywoodward88@gmail.com>
2023-12-06 09:31:27 +1100

    Add project files.

---
+++ MedicalPractice/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MedicalPractice
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ MedicalPractice/Pages/Components/AppointmentModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalPractice.Pages.Components
+{
+    public class AppointmentModel
+    {
+        [Required(ErrorMessage = "Patient Name is required.")]
+        public string PatientName { get; set; }
+
+        [Required(ErrorMessage = "Appointment Date is required.")]
+        public DateTime AppointmentDate { get; set; }
+
+    }
+}
